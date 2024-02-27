b6fe8712edcd58488b1b9838ebe283d05250be6b
unknown <Administrador@BOGDFPCGMF4135.FormacionSena.RED>
2024-02-27 16:49:23 -0500

    entidades y relaciones terminadas

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app , db)
 
 #importar modelos
-from .models import Medico  
+from .models import Medico , Paciente , Consultorio , Cita 
 
 #ejecutar objeto 
 if __name__ == "__main__":
--- pacientes_2826502.sql
+++ pacientes_2826502.sql
@@ -81,3 +81,4 @@ INSERT INTO `pacientes` (`id_pacientes`, `nombre`, `apellido`, `edad`) VALUES
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+information_schemamysql
